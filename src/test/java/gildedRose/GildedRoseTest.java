697e1a529869a65330ec091b02241b2fe258bb53
ALVARLEUNG <alvar.leung@qq.com>
2019-08-12 19:46:37 +0800

    test: add test[should_return_item_quality_0_and_sellIn_0_given_item_has_quality_0_and_sellIn_1] in GildedRoseTest

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -16,4 +16,14 @@ public class GildedRoseTest {
         assertEquals(0, item.sellIn);
     }
 
+    @Test
+    public void should_return_item_quality_0_and_sellIn_0_given_item_has_quality_0_and_sellIn_1() {
+        Item item = new Item("Rose", 1, 0);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+        gildedRose.updateQuality();
+
+        assertEquals(0, item.quality);
+        assertEquals(0, item.sellIn);
+    }
+
 }
